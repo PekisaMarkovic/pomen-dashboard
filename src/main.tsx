d4c82241ae794928323d3febf6eb6971fde009f4
Petar Markovic <petarmarkovic@Petars-MacBook-Pro.local>
2024-10-14 21:03:24 +0200

    chore: fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
 import { StrictMode } from 'react'
-import 'react-datepicker/dist/react-datepicker.css'
 import { createRoot } from 'react-dom/client'
-import 'react-toastify/dist/ReactToastify.css'
+import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { store } from './state/store.ts'
+import 'react-datepicker/dist/react-datepicker.css'
 import 'reset-css'
+import 'react-toastify/dist/ReactToastify.css'
 import './styles/index.scss'
+import { ToastContainer } from 'react-toastify'
 import './translations/config.ts'
+import MainModal from './components/modal/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div>MRS TAMO SISO</div>
+    <Provider store={store}>
+      <Router>
+        <App />
+        <ToastContainer autoClose={3000} position="top-center" hideProgressBar />
+        <MainModal />
+      </Router>
+    </Provider>
   </StrictMode>,
 )
