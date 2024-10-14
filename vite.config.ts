3213baf40c6b9a5ab05e2921566ce1723ef09958
Petar Markovic <petarmarkovic@Petars-MacBook-Pro.local>
2024-10-14 16:26:33 +0200

    Map ports

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   plugins: [react(), tsConfigPaths(), svgr({ svgrOptions: { expandProps: 'end' } })],
+  server: {
+    host: '0.0.0.0', // Expose the server to external connections
+    port: 5173, // Ensure the correct port is being used
+  },
 })
