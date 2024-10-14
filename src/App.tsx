1703f841bcfa28fa90bdbddffefc11fe3d2b7bb9
Petar Markovic <petarmarkovic@Petars-MacBook-Pro.local>
2024-10-14 20:16:42 +0200

    chore: fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Fragment } from 'react/jsx-runtime'
 import { useAuth } from './hooks/use-auth'
 import AppRoutes from './routes/AppRoutes'
 
 function App() {
   const { isLoad } = useAuth()
 
-  return isLoad ? <AppRoutes /> : null
+  return isLoad ? <AppRoutes /> : <Fragment />
 }
 
 export default App
