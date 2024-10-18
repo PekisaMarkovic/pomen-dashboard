279157c8562870de1651f39d52f3af0bdd742d89
Petar Markovic <petarmarkovic@Petars-MacBook-Pro.local>
2024-10-18 17:53:57 +0200

    chore: add logos

--- src/layouts/partials/LeftMenu.tsx
+++ src/layouts/partials/LeftMenu.tsx
@@ -5,6 +5,7 @@ import Heading from '../../components/core/typography/Heading'
 import { LEFT_SIDE_MENU_LINKS } from '../../constatns/navigation'
 import { checkIsLinkActiv } from '../../utils/route'
 import NavigationIcons from '../../icons/navigation'
+import Logo from '../../icons/Logo'
 
 const LeftMenu = () => {
   const { t } = useTranslation(['static'])
@@ -12,7 +13,9 @@ const LeftMenu = () => {
 
   return (
     <div className="py-8 px-5 flex flex-col gap-y-8 border-r-1 border-r-lighy-grey border-solid h-full w-full">
-      <div className="px-0.5 flex items-baseline">POMEN LOGO</div>
+      <div className="px-0.5 flex items-baseline">
+        <Logo type="DARK" />
+      </div>
       <div className="px-0.5 flex flex-col gap-y-6">
         <Heading text={t('static:navigation.leftSide.title')} variant="2" size="base" color="grey" />
         <div className="flex flex-col gap-y-4">
