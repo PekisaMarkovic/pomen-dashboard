d52b64e0289bdd4bfb96f23ea1c83810b8d38a17
Petar Markovic <petarmarkovic@Petars-MacBook-Pro.local>
2025-02-11 19:47:37 +0100

    chore: check screen size and limit usage

--- src/components/section/ScreenSizeWarnings.tsx
+++ src/components/section/ScreenSizeWarnings.tsx
@@ -1,22 +1,33 @@
+import { PropsWithChildren, useEffect, useState } from 'react'
 import Paragraph from '@/src/components/core/typography/Paragraph'
 import Logo from '@/src/icons/Logo'
-import { PropsWithChildren } from 'react'
 import { useTranslation } from 'react-i18next'
 
 const ScreenSizeWarning = ({ children }: PropsWithChildren) => {
+  const [showWarning, setShowWarning] = useState(false)
   const { t } = useTranslation(['g'])
 
-  return (
-    <>
-      <div className="hidden md:block">{children}</div>
-      <div className="block md:hidden fixed top-0 left-0 h-full w-full flex items-center justify-center bg-green ">
-        <div className="w-1/2 flex items-center justify-center flex-col">
-          <Logo type="WHITE" />
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setShowWarning(window.innerWidth < 768)
+    }
 
-          <Paragraph size="lg" color="white" className="text-center mt-8" text={t('g:screenWraning')} />
-        </div>
+    checkScreenSize()
+    window.addEventListener('resize', checkScreenSize)
+
+    return () => window.removeEventListener('resize', checkScreenSize)
+  }, [])
+
+  if (!showWarning) return children
+
+  return (
+    <div className="fixed top-0 left-0 h-full w-full flex items-center justify-center bg-green ">
+      <div className="w-1/2 flex items-center justify-center flex-col">
+        <Logo type="WHITE" />
+
+        <Paragraph size="lg" color="white" className="text-center mt-8" text={t('g:screenWraning')} />
       </div>
-    </>
+    </div>
   )
 }
 export default ScreenSizeWarning
